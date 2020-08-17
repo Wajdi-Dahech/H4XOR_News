74a546e591c7b225dd550fe2c8aed60b0075289b
D.Wajdi <wajdi.dahech@esprit.tn>
2020-08-17 05:38:09 -0700

    .

--- H4XOR News/Views/DetailView.swift
+++ H4XOR News/Views/DetailView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DetailView.swift
 //  H4XOR News
 //
 //  Created by Katsu on 8/4/20.
@@ -8,14 +8,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct DetailView: View {
+    
+    let url: String?
     var body: some View {
-        Text("Hello, World!")
+        WebView(urlString: url)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DetailView(url: "https://www.google.com")
     }
 }
+
+
---
+++ README.md
@@ -0,0 +1,17 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+   <img src="ScreenShot/logo.png" alt="Logo" width="100" height="100">
+  <h2 align="center">H4XOR News</h2>
+</p>
+<p align="center">
+A Hacker News reader, get hold of the latest articles that are trending using the Hacker News API.
+</p>
+<br>
+
+![port-git](ScreenShot/ScreenShot.jpg)
+
+### Technologies Used
+
+`Swift 5`  `SwiftUI`  `Hacker News API`  `WebKit`
+
